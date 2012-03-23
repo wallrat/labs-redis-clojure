6368a4697685691d8a6a9b7f5ec691465d821a78
Andreas Bielk <andreas@bielk.se>
2012-03-23 04:08:06 +0100

    Prepare for github

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject labs.redis "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[clojure-source "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.1"]]
+  :dev-dependencies [[clojure-source "1.3.0"]
+                     [criterium "0.2.0"]]
   :java-source-path "java"
   :main labs.redis.core)
